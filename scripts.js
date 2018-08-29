d9d59efbe8f130356662eabf90183066ebd1f31d
Andy Zalit <andyzalit@gmail.com>
2018-08-29 16:16:10 -0700

    Get return data into window

--- scripts.js
+++ scripts.js
@@ -1,19 +1,62 @@
+// x - User selects game setup: #of q's, category, difficulty, question style
+// x - Reset button set page to defaults
+// Play button makes Get request and sets up trivia cards
+  // Place trivia data into array. display first cards
+// Answer button hilites answer
+// Next question button pulls next question from index
+
 let baseURL = 'https://opentdb.com/api.php?'
 
 
 let formInputs = document.querySelector('#playButton');
-document.querySelector('form').addEventListener('submit', () => {
+
+document.querySelector('#playButton').addEventListener('click', () => {
   const triviaAmount = document.querySelector('#trivia-amount');
   const category = document.querySelector('#topic');
-  const difficulty = document.querySelector('#difficulty');
-  const questionStyle = document.querySelector('#multi-boolean');
+  let difficulty = '';
+  let questionStyle = '';
   const triviaCard = document.querySelector('#trivia-card');
+  const multi = document.querySelector('#multi');
+  const tf = document.querySelector('#TF');
+  const easy = document.querySelector('#easy');
+  const medium = document.querySelector('#medium');
+  const hard = document.querySelector('#hard');
   event.preventDefault();
 
-  axios.get(`${baseURL}amount=${triviaAmount.value}&category=${category.value}&difficulty=${difficulty.value}&type=${questionStyle.value}`)
+if(easy.checked === true) {
+  difficulty = 'easy';
+} else if(medium.checked === true) {
+    difficulty = 'medium';
+} else {
+  difficulty = 'hard';
+}
+
+  if(multi.checked === true) {
+    questionStyle = 'multiple';
+  } else {
+    questionStyle = 'boolean';
+  }
+  //https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple
+
+  console.log(tf.checked)
+
+  axios.get(`${baseURL}amount=${triviaAmount.value}&category=${9}&difficulty=${difficulty}&type=${questionStyle}`)
   .then(trivResult => {
+    console.log(trivResult)
     triviaCard.innerHTML = `
-    <code>${JSON.stringify(triviaCard.data)}</code>
+    <code>${JSON.stringify(trivResult.data.results)}</code>
     `
   })
 })
+
+// let rawTrivia = triviaCard.data
+//
+// function createCard() {
+//   let card = [];
+//   for(let i = 0; i < rawTrivia.length; i++) {
+//     console.log(rawTrivia);
+//     card.push(rawTrivia);
+//   }
+//   return card;
+// }
+// console.log(createCard);
