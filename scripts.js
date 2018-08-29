1cdf947c880b3384acb96c8dca7a8219312e81b5
Andy Zalit <andyzalit@gmail.com>
2018-08-29 13:26:08 -0700

    triviaCard window active

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
-const baseUrl = 'https://opentdb.com/api.php?'
+let baseURL = 'https://opentdb.com/api.php?'
+
 
 let formInputs = document.querySelector('#playButton');
-document.querySelector('form').addEventListener('submit', function(event){
+document.querySelector('form').addEventListener('submit', () => {
+  const triviaAmount = document.querySelector('#trivia-amount');
+  const category = document.querySelector('#topic');
+  const difficulty = document.querySelector('#difficulty');
+  const questionStyle = document.querySelector('#multi-boolean');
+  const triviaCard = document.querySelector('#trivia-card');
   event.preventDefault();
-  if formInputs.value){
 
-  }
+  axios.get(`${baseURL}amount=${triviaAmount.value}&category=${category.value}&difficulty=${difficulty.value}&type=${questionStyle.value}`)
+  .then(trivResult => {
+    triviaCard.innerHTML = `
+    <code>${JSON.stringify(triviaCard.data)}</code>
+    `
+  })
+})
