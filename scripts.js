8e4f7c9e77fb462c09dbd2a8ff332d11c2bef796
Andy Zalit <andyzalit@gmail.com>
2018-08-28 17:18:37 -0700

    add Q type start logic

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const baseUrl = 'https://opentdb.com/api.php?'
+
+let formInputs = document.querySelector('#playButton');
+document.querySelector('form').addEventListener('submit', function(event){
+  event.preventDefault();
+  if formInputs.value){
+
+  }
